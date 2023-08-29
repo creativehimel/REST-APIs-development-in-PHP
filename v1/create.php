92ce9248e1ed7f7be16bc4e7d707c821599f3ca3
Md. Himel Ali <himel.phy160@gmail.com>
2023-08-30 01:00:13 +0600

    Insert all condition in create.php

--- v1/create.php
+++ v1/create.php
@@ -1,4 +1,8 @@
 <?php
+    // Include Header
+    header("Access-Control-Allow-Origin: *"); //it allows all origin like localhost, any domain or any subdomain
+    header("Content-type: application/json charset = UTF-8"); //Data which we are getting inside request
+    header("Access-Control-Allow-Method: POST"); // Method types allow
     // Include database.php;
     include_once("../config/database.php");
 
@@ -13,16 +17,37 @@
     $student = new Student($connection);
 
     if ($_SERVER['REQUEST_METHOD'] === "POST") {
-        // Submit data
-        $student->name = "Sanjay";
-        $student->email = "sanjay@gmail.com";
-        $student->phone = "01625458564";
-        if ($student->createData()) {
-            echo "Student has been created";
+        $data = json_decode(file_get_contents("php://input"));
+        if (!empty($data->name) && !empty($data->email) && !empty($data->phone)) {
+            // Submit data
+            $student->name = $data->name;
+            $student->email = $data->email;
+            $student->phone = $data->phone;
+            if ($student->createData()) {
+                http_response_code(200); // 200 means Ok
+                echo json_encode(array(
+                    "status" => 1,
+                    "message" => "Student has been created"
+                ));
+            }else{
+                http_response_code(500); // 500 means internal server error
+                echo json_encode(array(
+                    "status" => 0,
+                    "message" => "Failed to created student"
+                ));
+            }
         }else{
-            echo "Failed to insert data.";
+            http_response_code(404); // 404 means page not found
+            echo json_encode(array(
+                "status" => 0,
+                "message" => "All Values needed"
+            ));
         }
     }else{
-        echo "Access denied";
+        http_response_code(503); // 500 means service unavailable
+        echo json_encode(array(
+            "status" => 0,
+            "message" => "Access denied"
+        ));
     }
 
